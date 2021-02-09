5d8136ef31d117dc6f75a3a1179423fc1e4b2d8c
LedisHzj <lxotzai013@gmail.com>
2021-02-09 17:35:29 +0200

    add some routes

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import { Button, Select } from "element-ui";
+import lang from "element-ui/lib/locale/lang/en";
+import locale from "element-ui/lib/locale";
+import "@/assets/element-variables.scss";
 
-Vue.config.productionTip = false
+Vue.prototype.$ELEMENT = { size: "small", zIndex: 3000 };
+
+// configure language
+locale.use(lang);
+
+Vue.use(Button);
+Vue.use(Select);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
---
+++ src/views/Form.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Form with vuejs and Element ui</h1>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+}
+</script>

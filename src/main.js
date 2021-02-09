178c7286dc949b663a0c53bc933b33fce48981f4
LedisHzj <lxotzai013@gmail.com>
2021-02-09 19:19:42 +0200

    Add form

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import { Button, Select } from "element-ui";
+import {Row, Col, Button, Select, Option, Form, FormItem, Radio, RadioGroup, Input, Checkbox, Divider, Card, Switch, CheckboxGroup} from "element-ui";
 import lang from "element-ui/lib/locale/lang/en";
 import locale from "element-ui/lib/locale";
 import "@/assets/element-variables.scss";
@@ -12,8 +12,21 @@ Vue.prototype.$ELEMENT = { size: "small", zIndex: 3000 };
 // configure language
 locale.use(lang);
 
+Vue.use(Row);
+Vue.use(Col);
 Vue.use(Button);
 Vue.use(Select);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Option);
+Vue.use(Radio);
+Vue.use(RadioGroup);
+Vue.use(Input);
+Vue.use(Checkbox);
+Vue.use(CheckboxGroup);
+Vue.use(Divider);
+Vue.use(Card);
+Vue.use(Switch);
 
 Vue.config.productionTip = false;
 
